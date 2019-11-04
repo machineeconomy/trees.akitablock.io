797ca9a9401fafdf6b773a5af7c5bccccf38d75b
huhn <sehe89@gmail.com>
2019-11-04 22:07:19 +0100

    add website

---
+++ README.md
@@ -0,0 +1,28 @@
+# trees.akitablock.io
+
+> My geometric Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+## images
+https://www.pexels.com/photo/green-pine-trees-1563604/
+Photo by thiago japyassu from Pexels
+
+<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
---
+++ pages/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="container">
+    <div>
+      <img class="qr-code" v-if="qrCodeData" :src="qrCodeData.src" alt="QR CODE" />
+      <div>
+      <a :href="trinity_link" class="action button--green">Donate with Trinity</a>
+      </div>
+      <div class="box">
+        <h1 class="title">#iotatrees</h1>
+        <h2 class="subtitle">Plant a 🌳 with IOTA</h2>
+        <div class="logos">
+          <img class="logo logo--akita" src="~/assets/AKITALogo.png" alt />
+          <img class="logo logo--iota" src="~/assets/IOTALogo.png" alt />
+        </div>
+        <div class="links">
+          <a href="https://teamtrees.org/" target="_blank" class="button button--green">Learn more</a>
+          <a href="https://thetangle.org/address/OQYZHELDOEEA9ZYIKHZRHZQDORNAEPMYWJ9KSNDOUDNAKWLGYSEQUQVXR9UYWQRJLCZSKT99L9ZTHDQNW" target="_blank" class="button button--grey">Balance on TheTangle.org</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as IotaQR from "@tangle-frost/iota-qr-lib/pkg/iota-qr-lib.js";
+
+export default {
+  components: {},
+  data() {
+    return {
+      qrCodeData: null,
+      address:
+        "OQYZHELDOEEA9ZYIKHZRHZQDORNAEPMYWJ9KSNDOUDNAKWLGYSEQUQVXR9UYWQRJLCZSKT99L9ZTHDQNWUUWJUK9BD",
+        message: "Donated on https://trees.akitablock.io by <anonymous>",
+        value: 5000000
+    };
+  },
+  computed: {
+    trinity_link() {
+      return "iota://" + this.address + "/?amount=" + this.value + "&message=" + this.message
+    }
+  },
+  created() {
+    const paymentData = IotaQR.TrinityPaymentQR.generatePaymentData(
+                    this.address,
+                    this.value,
+                    "AKITA",
+                    this.message);
+    IotaQR.TrinityPaymentQR.renderHtml(paymentData, "png", 11, 4, 0,
+        {
+            background: new IotaQR.Color(1, 0, 0, 0)
+        }).then(
+            qrCodeData => {
+              this.qrCodeData = qrCodeData;
+              console.log("qr_code_data", qrCodeData);
+              console.log("qr_code_data", qrCodeData.src);
+            }
+          );
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  background-image: url("../assets/forrest.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  background-color: #999;
+}
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.action {
+  color: white;
+  background: #3b8070;
+  margin: 20px;
+  &:hover {
+    background: #2ba085;
+  }
+}
+
+.qr-code {
+  margin-top: 30px;
+}
+.box {
+  background: rgba(0, 0, 0, 0.4); 
+  color: white;
+  border-radius: 10px;
+  transition: background 1s;
+  padding: 20px;
+}
+
+.logos {
+  padding-bottom: 30px;
+  .logo {
+    &--akita {
+      height: 80px;
+    }
+    &--iota {
+      height: 60px;
+      margin: 10px;
+    }
+  }
+}
+
+.title {
+  font-family: "Open Sans", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #efefef;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+
+  font-family: "Open Sans", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-weight: 300;
+  font-size: 42px;
+  color: #efefef;
+  word-spacing: 5px;
+  padding: 20px;
+}
+
+.links {
+  padding-top: 15px;
+}
+.button {
+  color: #efefef;
+}
+</style>
